2f106c6504d80dc58966f13a01580ad07128d492
nicklasstellberg <nicklas.stellberg@hotmail.com>
2023-02-03 14:13:23 +0200

    Enemy behaviour

---
+++ Assets/Scripts/AiChase.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiChase : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    public float distanceBetween;
+
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+
+        if(distance < distanceBetween)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+    }
+}
