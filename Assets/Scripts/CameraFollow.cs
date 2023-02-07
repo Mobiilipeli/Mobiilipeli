515d9fb1b616983a7125285835ab730ce3f1da56
nicklasstellberg <nicklas.stellberg@hotmail.com>
2023-02-07 15:28:26 +0200

    Camera with follow and bounds

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    [Range(1,10)]
+    public float smoothFactor;
+    public Vector3 minValues, maxValues;
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+    void Follow()
+    {
+        //Define minimum x,y,z values and maximum x,y,z values
+
+
+        Vector3 targetPosition = target.position + offset;
+        //Verify if the targetPosition is out of bound or not
+        //Limit it to the min and max values
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x, minValues.x, maxValues.x),
+            Mathf.Clamp(targetPosition.y, minValues.y, maxValues.y),
+            Mathf.Clamp(targetPosition.z, minValues.z, maxValues.z));
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothFactor*Time.fixedDeltaTime);
+        transform.position = smoothPosition;
+    }
+}
