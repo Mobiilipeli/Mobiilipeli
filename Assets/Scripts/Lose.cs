db6038b05e0881219b8cc2c86ea64a0841e7e527
Paantee <teemu-paananen@hotmail.com>
2023-03-07 12:19:23 +0200

    victoryflag and spikes test

--- Assets/Scripts/Lose.cs
+++ Assets/Scripts/Lose.cs
@@ -43,4 +43,11 @@ public class Lose : MonoBehaviour
 	        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 	    }
 	}
+	    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
 }
