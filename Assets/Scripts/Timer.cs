d5822d07ead19b13ff7bee586fd37919a1575b4f
Paantee <teemu-paananen@hotmail.com>
2023-03-29 12:28:50 +0300

    timer added

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+
+    [Header("Component")]
+    public TextMeshProUGUI timerText;
+
+    [Header("Timer Settings")]
+    public float currentTime;
+    public bool countDown;
+
+    [Header("Limit Settings")]
+    public bool hasLimit;
+    public float timerLimit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = countDown ? currentTime -= Time.deltaTime : currentTime += Time.deltaTime;
+
+        if (currentTime < 60)
+        {
+            currentTime = countDown ? currentTime -= Time.deltaTime : currentTime += Time.deltaTime;
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        SetTimerText();
+    }
+    private void SetTimerText()
+    {
+        timerText.text = currentTime.ToString("0.0");
+    }
+}
