ad4ae729518b6577ce6ec7acfbbec5c028702cd2
nicklasstellberg <nicklas.stellberg@hotmail.com>
2023-04-09 14:00:08 +0300

    Best times for levels

---
+++ Assets/Scripts/BestTimeText.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using TMPro;
+
+public class BestTimeText : MonoBehaviour
+{
+    public string timeKey; // Tallennusavain parhaalle ajalle
+
+    private void Start()
+    {
+        // Haetaan tallennettu aika ja asetetaan tekstikent‰n arvoksi
+        float bestTime = PlayerPrefs.GetFloat(timeKey, Mathf.Infinity); // Jos aikaa ei ole tallennettu, k‰ytet‰‰n Mathf.Infinity -arvoa
+        GetComponent<TextMeshProUGUI>().text = "Best Time: " + bestTime.ToString("0.00") + "s";
+    }
+}
--- Assets/Scripts/MoveToNextLevel.cs
+++ Assets/Scripts/MoveToNextLevel.cs
@@ -2,42 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MoveToNextLevel : MonoBehaviour
 {
-    public int nextSceneLoad; // Seuraavan tason buildindeksi.
+    public int nextSceneLoad;
+    private Dictionary<int, float> levelTimes; // 1. Create a dictionary to hold the completion times for all levels.
+    public TextMeshProUGUI bestTimeText; // 2. Declare a UI Text object to display the best completion time.
 
-    // Start-funktio kutsutaan ennen ensimmäistä framea.
     void Start()
     {
-        // Haetaan seuraavan tason buildindeksi Scene Managerilta.
         nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
+        levelTimes = new Dictionary<int, float>(); // 3. Initialize the dictionary.
+
+        // 4. Load all saved completion times into the dictionary.
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+            float completionTime = PlayerPrefs.GetFloat("levelTime" + i.ToString(), 0f);
+            levelTimes.Add(i, completionTime);
+        }
+
+        // 5. Retrieve and display the best completion time for the current scene.
+        float bestTime = levelTimes[SceneManager.GetActiveScene().buildIndex];
+        if (bestTime > 0)
+        {
+            bestTimeText.text = "Best Time: " + bestTime.ToString("F2") + "s";
+        }
+        else
+        {
+            bestTimeText.text = "No Best Time Set Yet";
+        }
     }
 
-    // Tämä metodi kutsutaan, kun pelaaja osuu "triggeriin" (laatikkoon, johon on kiinnitetty "trigger"-komponentti).
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player") // Jos pelaaja osuu triggeriin...
+        if (collision.tag == "Player")
         {
-            if (SceneManager.GetActiveScene().buildIndex == 7) /* < Muuta tämän kokonaislukuarvon
-                                                                   arvo vastaamaan viimeisen tason
-                                                                   buildindeksiäsi Build Settings -ikkunassa. */
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            float currentTime = Time.timeSinceLevelLoad;
+            float bestTime = levelTimes[currentSceneIndex];
+            if (bestTime == 0 || currentTime < bestTime)
             {
-                Debug.Log("You Completed ALL Levels"); // Tulostetaan konsoliin ilmoitus siitä, että kaikki tasot on läpäisty.
-
-                // Näytetään voittoruutu tms.
+                levelTimes[currentSceneIndex] = currentTime;
+                PlayerPrefs.SetFloat("levelTime" + currentSceneIndex.ToString(), currentTime);
+                Debug.Log("Level " + currentSceneIndex.ToString() + " new best time: " + currentTime.ToString());
             }
             else
             {
-                // Siirrytään seuraavalle tasolle.
+                Debug.Log("Level " + currentSceneIndex.ToString() + " time: " + currentTime.ToString() + " not better than previous best time: " + bestTime.ToString());
+            }
+
+            if (currentSceneIndex == SceneManager.sceneCountInBuildSettings - 1)
+            {
+                Debug.Log("You Completed ALL Levels");
+
+                // Show victory screen or perform other end-game actions.
+            }
+            else
+            {
                 SceneManager.LoadScene(nextSceneLoad);
 
-                // Asetetaan tallennettu indeksi seuraavalle tasolle.
                 if (nextSceneLoad > PlayerPrefs.GetInt("levelAt"))
                 {
                     PlayerPrefs.SetInt("levelAt", nextSceneLoad);
                 }
             }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++) // 7. Print the completion times for all levels in the dictionary.
+        {
+            Debug.Log("Level " + i.ToString() + " time: " + levelTimes[i].ToString());
         }
     }
 }
